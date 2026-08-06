5be40cf90062ff9cae9bba40e7cd27aec7f37f1f
theworkco <github@theworkco.com>
2026-08-06 17:56:54 -0400

    Remove incorrectly encoded Studio metadata route

--- app/studio/%5B%5B...tool%5D%5D/layout.tsx
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: 'Content Studio | theworkco',
-  robots: {index: false, follow: false},
-}
-
-export default function StudioLayout({children}: {children: React.ReactNode}) {
-  return children
-}
